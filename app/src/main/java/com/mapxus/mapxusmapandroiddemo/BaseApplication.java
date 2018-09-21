02ea33e0a331687a8a70083fca3f01331bdbb01d
Ethan Wang <ethanwang@mapxus.com>
2018-09-21 17:19:10 +0800

    add tencent bugly auto upgrade

--- app/src/main/java/com/mapxus/mapxusmapandroiddemo/BaseApplication.java
+++ app/src/main/java/com/mapxus/mapxusmapandroiddemo/BaseApplication.java
@@ -3,14 +3,14 @@ package com.mapxus.mapxusmapandroiddemo;
 import android.support.multidex.MultiDexApplication;
 
 import com.mapxus.map.MapxusMapContext;
-import com.tencent.bugly.crashreport.CrashReport;
+import com.tencent.bugly.Bugly;
 
 public class BaseApplication extends MultiDexApplication {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        CrashReport.initCrashReport(getApplicationContext(), "4baac78a3f", true);
+        Bugly.init(getApplicationContext(), "4baac78a3f", true);
         MapxusMapContext.init(getApplicationContext());
     }
 }
