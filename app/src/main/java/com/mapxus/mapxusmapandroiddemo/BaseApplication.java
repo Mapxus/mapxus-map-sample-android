9e566c04b46a7e67a8ff3fdac8d64f9d65019be6
Ethan Wang <ethanwang@mapxus.com>
2018-10-26 16:30:58 +0800

    add plant timber log tree

--- app/src/main/java/com/mapxus/mapxusmapandroiddemo/BaseApplication.java
+++ app/src/main/java/com/mapxus/mapxusmapandroiddemo/BaseApplication.java
@@ -5,6 +5,8 @@ import android.support.multidex.MultiDexApplication;
 import com.mapxus.map.MapxusMapContext;
 import com.tencent.bugly.Bugly;
 
+import timber.log.Timber;
+
 public class BaseApplication extends MultiDexApplication {
 
     @Override
@@ -12,5 +14,9 @@ public class BaseApplication extends MultiDexApplication {
         super.onCreate();
         Bugly.init(getApplicationContext(), "4baac78a3f", true);
         MapxusMapContext.init(getApplicationContext());
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+
     }
 }
